caec31763b7a58841a1bd9935be479923c1883b6
Tharle Josefi de Camargo <tharles.josefidecamargo@gmail.com>
2023-11-20 09:34:04 -0500

    Scene1: 11.6. Jump.

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -1,13 +1,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor.Experimental.GraphView;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float m_Speed = 10;
-    [SerializeField] float m_JumpForce= 5;
+    private const float DISTANCE_CHECK_COLLISION_GROUND = 1f;
+
+    [SerializeField] float m_Speed = 2;
+    [SerializeField] float m_JumpForce= 15;
+    [SerializeField] Transform m_Foot;
     private Rigidbody m_RigidBody;
 
     // Start is called before the first frame update
@@ -25,7 +30,7 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (Input.GetKeyDown(GameParametres.InputName.KEY_JUMP))
+        if (IsGround() && Input.GetKeyDown(GameParametres.InputName.KEY_JUMP))
         {
             m_RigidBody.AddForce(Vector3.up * m_JumpForce, ForceMode.Impulse);
         }
@@ -37,5 +42,15 @@ public class PlayerController : MonoBehaviour
         float axisVertical = Input.GetAxis(GameParametres.InputName.AXIS_VERTICAL);
         Vector3 direction = Vector3.right * axisHorizontal + Vector3.forward * axisVertical;
         m_RigidBody.AddForce(direction * m_Speed, ForceMode.Force);
+    }
+
+    private bool IsGround()
+    {
+        if(Physics.Raycast(m_Foot.position, Vector3.down, out RaycastHit hitInfo, DISTANCE_CHECK_COLLISION_GROUND))
+        {
+            return hitInfo.collider.CompareTag(GameParametres.TagName.GROUND);
+        }
+
+        return false;
     }
 }
