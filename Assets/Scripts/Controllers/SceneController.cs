8046e35d205ac35c33371c4749dcb72dadaebef8
Tharle Josefi de Camargo <tharles.josefidecamargo@gmail.com>
2023-11-20 12:08:19 -0500

    Add Menu for change scenes

--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -9,4 +9,29 @@ public class SceneController : MonoBehaviour
     {
         SceneManager.LoadScene(GameParametres.SceneName.SCENE_1);
     }
+
+    public void OnChangeScene2()
+    {
+        SceneManager.LoadScene(GameParametres.SceneName.SCENE_2);
+    }
+
+    public void OnChangeScene3()
+    {
+        SceneManager.LoadScene(GameParametres.SceneName.SCENE_3);
+    }
+
+    public void OnChangeScene4()
+    {
+        SceneManager.LoadScene(GameParametres.SceneName.SCENE_4);
+    }
+
+    public void OnChangeScene5()
+    {
+        SceneManager.LoadScene(GameParametres.SceneName.SCENE_5);
+    }
+
+    public void OnQuit()
+    {
+        Application.Quit();
+    }
 }
